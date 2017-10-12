e1e6c6cbad213d2aa5221c1cee532f58135a35cf
Jim Flynn <jflynn129@gmail.com>
2017-10-12 16:11:30 -0400

    correcting comments and general cleanup

--- fact_script.bat
+++ fact_script.bat
@@ -6,35 +6,29 @@
 ::  if everything is correct, installs the scripts that autopower up
 ::  the quick-start application when powered up.
 ::
-::  fact_script QCOM_BOOT_PORT  PATH_TO_FACT_SCRIPTS
+::  fact_script PATH_TO_IOT_MONITOR
 :: 
 
 setlocal ENABLEDELAYEDEXPANSION
-
-set QUIET=^>NUL
-set LOUD=
-
-set VERBOSE=%QUIET%
 
 set GSKDEV=a2e26b03f4e77aab23dbc5294b277d69
 set PRODEV=4d074ddd8656173b0c4b29d2ac45ccad
 
 set APIKEY=%GSKDEV%
+
 set FSSRC=%1
 
 if "%FSSRC%" EQU "" (
-@echo "You must provide a path to the FACTORY_SCRIPTS"
+@echo "You must provide a path to the IOT_MONITOR application"
 goto err
 ) 
 
 if not exist %FSSRC% (
-@echo "You must provide a valid path to the FACTORY_SCRIPTS"
+@echo "You must provide a valid path to the IOT_MONITOR application"
 goto err
 ) 
 
 @echo ^>^>Loading iot_monitor from %FSSRC%
-@echo .
-@echo .
 @echo .
 
 @echo ^>^> 1.Flash program the firmware (if needed)
@@ -45,8 +39,6 @@ call :flash
 adb wait-for-device
 
 @echo .
-@echo .
-@echo .
 
 :: -------------------------------------------------
 echo ^>^> 3.Put the Iot_monitor program onto the device
@@ -55,8 +47,6 @@ adb shell "mkdir -p /CUSTAPP/iot"
 adb push %FSSRC%/iot_monitor /CUSTAPP/iot/iot_monitor
 adb shell "chmod +x /CUSTAPP/iot/iot_monitor"
 
-@echo .
-@echo .
 @echo .
 
 :: -------------------------------------------------
@@ -67,8 +57,6 @@ SET /P ans="Did it run successfully (Y/N)?"
 if "%ans%" NEQ "Y" if "%ans%" NEQ "y" goto err
 
 @echo .
-@echo .
-@echo .
 
 :: -------------------------------------------------
 @echo ^>^> 2.Update the malmanager.cfg file to have the correct APN set
@@ -78,8 +66,6 @@ adb shell "mv /CUSTAPP/user/mm_conf/malmanager.cfg  /CUSTAPP/user/mm_conf/malman
 adb shell "while [ ! -f = /CUSTAPP/user/mm_conf/malmanager.old_cfg ]; do sleep 1; done;"
 adb shell "cat /CUSTAPP/user/mm_conf/malmanager.old_cfg | sed 's/\<name\>\ =.*;/name\ =\ \"m2m.com.attz\";/' > /CUSTAPP/user/mm_conf/malmanager.cfg"
 
-@echo .
-@echo .
 @echo .
 
 :: -------------------------------------------------
